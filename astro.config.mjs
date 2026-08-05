51840bb4b697cf52483f9dfe616bc639e7a6dc5c
gcabalceta <gcabalceta@cedesdonbosco.ed.cr>
2026-08-05 14:31:00 -0600

    fix: configure Astro static deployment for Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,32 @@
+// import { defineConfig } from 'astro/config';
+// import tailwind from "@astrojs/tailwind";
+// import svelte from "@astrojs/svelte";
+// import react from "@astrojs/react";
+
+// import netlify from '@astrojs/netlify';
+
+// // https://astro.build/config
+// export default defineConfig({
+//   integrations: [tailwind(), svelte(), react()],
+//   output: 'server',
+//   adapter: netlify(),
+//   devToolbar: {
+//     enabled: false
+//   }
+// });
+
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import svelte from "@astrojs/svelte";
-import react from "@astrojs/react";
+import tailwind from '@astrojs/tailwind';
+import svelte from '@astrojs/svelte';
+import react from '@astrojs/react';
 
-import netlify from '@astrojs/netlify';
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), react()],
-  output: 'server',
-  adapter: netlify(),
+  integrations: [
+    tailwind(),
+    svelte(),
+    react()
+  ],
+
   devToolbar: {
     enabled: false
   }
