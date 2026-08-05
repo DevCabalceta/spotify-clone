2c9da6f9e498420a3d21d2e9a63cfc58edb84217
gcabalceta <gcabalceta@cedesdonbosco.ed.cr>
2026-08-05 15:07:32 -0600

    Upgrade Astro 7 and migrate to Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+// import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 // import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), react()],
+  integrations: [svelte(), react()],
   output: 'server',
   // adapter: netlify(),
   adapter: vercel(),
