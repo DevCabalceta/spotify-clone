bd9713e46bb99e66559ea19ac973f80a55a113cc
gcabalceta <gcabalceta@cedesdonbosco.ed.cr>
2026-08-05 14:40:12 -0600

    Migrate from Netlify to Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,18 @@
-// import { defineConfig } from 'astro/config';
-// import tailwind from "@astrojs/tailwind";
-// import svelte from "@astrojs/svelte";
-// import react from "@astrojs/react";
-
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
+import react from "@astrojs/react";
+import vercel from '@astrojs/vercel/serverless';
 // import netlify from '@astrojs/netlify';
 
-// // https://astro.build/config
-// export default defineConfig({
-//   integrations: [tailwind(), svelte(), react()],
-//   output: 'server',
-//   adapter: netlify(),
-//   devToolbar: {
-//     enabled: false
-//   }
-// });
-
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import svelte from '@astrojs/svelte';
-import react from '@astrojs/react';
-
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    svelte(),
-    react()
-  ],
-
+  integrations: [tailwind(), svelte(), react()],
+  output: 'server',
+  // adapter: netlify(),
+  adapter: vercel(),
   devToolbar: {
     enabled: false
   }
 });
+
